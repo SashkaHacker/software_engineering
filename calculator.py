1f70e01658a33a9ec33cd3858d9381778ecbe992
Матвеев <am942081@gmail.com>
2023-09-12 23:30:58 +0300

    add func build

--- calculator.py
+++ calculator.py
@@ -5,3 +5,30 @@ class Main(Frame):
     def __init__(self, root):
         super(Main, self).__init__(root)
         self.build()
+
+    def build(self):
+        self.formula = "0"
+        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
+        self.lbl.place(x=11, y=50)
+
+        btns = [
+            "C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "(", "0", ")", "X^2"
+        ]
+
+        x = 10
+        y = 140
+        for bt in btns:
+            com = lambda x=bt: self.logicalc(x)
+            Button(text=bt, bg="#FFF",
+                   font=("Times New Roman", 15),
+                   command=com).place(x=x, y=y,
+                                      width=115,
+                                      height=79)
+            x += 117
+            if x > 400:
+                x = 10
+                y += 81
